8afb14e33ea8b86a4a092f397317604dd935612a
passionroro <88278592+passionroro@users.noreply.github.com>
2022-04-07 11:40:54 +0200

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -6,28 +6,13 @@
 /*   By: rohoarau <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 14:36:59 by rohoarau          #+#    #+#             */
-/*   Updated: 2022/04/06 16:33:28 by rohoarau         ###   ########.fr       */
+/*   Updated: 2022/04/06 23:49:42 by rohoarau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
 /*
-static char *line_read = (char *)NULL;
-
-char	*rl_gets(void)
-{
-	if (line_read)
-	{
-    	free (line_read);
-    	line_read = (char *)NULL;
-    }
-	line_read = readline ("[prompt]$ ");
-	if (line_read && *line_read)
-    	add_history (line_read);
-	return (line_read);
-}
-
-
 int	run_prompt(char *str, t_minishell sh)
 {
 	sh->prompt = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1));
@@ -37,14 +22,13 @@ int	run_prompt(char *str, t_minishell sh)
 
 void	signal_handler(int sig)
 {
-//	printf("signal: %d\n", sig);
+	//t_minishell	sh;
+
 	if (sig == 2)
-	{
-		write(1, "\n", 1);
-		ft_putstr_fd("[prompt]$ ", 1);
-	}
+		write(1, "\n[prompt]$ ", 11);
 	if (sig == 3)
 		return ;
+		//sh.line_read = "\0";
 }
 
 int	main(int argc, char **envp)
@@ -52,22 +36,20 @@ int	main(int argc, char **envp)
 	(void)envp;
 	int			pid;
 	int			exit;
-	//t_minishell	sh;
+	t_minishell	sh;
 
 	if (argc != 1)
 		return (1);
 	exit = 1;
 	pid = getpid();
-	signal(SIGINT, signal_handler);
-	signal(SIGQUIT, signal_handler);
 	while (exit)
 	{
-		//ft_putstr_fd("[prompt]$ ", 1);
-		//char	line[128];
-		char *line = NULL;
-		printf("%s\n", line);
-		//fgets(line, sizeof(line), stdin);
-		exit = ft_strncmp(line, "exit\n", sizeof(line));
+		signal(SIGINT, signal_handler);
+		signal(SIGQUIT, signal_handler);
+		sh.line_read = readline("[prompt]$ ");
+		printf("line_read	%s\n", sh.line_read);
+		exit = ft_strncmp(sh.line_read, "exit", sizeof(sh.line_read));
 	}
+	free(sh.line_read);
 	return (0);
 }
