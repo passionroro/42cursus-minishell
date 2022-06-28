07d790a982f5c1a7532410cfbc8a6feb42841d36
Romain Hoarau <rohoarau@student.42lausanne.ch>
2022-06-28 16:59:14 +0200

    coucou first commit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: henkaoua <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 19:06:25 by henkaoua          #+#    #+#             */
-/*   Updated: 2022/06/26 17:26:36 by rohoarau         ###   ########.fr       */
+/*   Updated: 2022/06/28 16:58:13 by rohoarau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ void	reset_saved_fd(t_minishell *sh, t_node *tmp)
 {
 	dup2(sh->saved_fd[0], 0);
 	close(sh->saved_fd[0]);
-	dup2(sh->saved_fd[1], 1);
-	close(sh->saved_fd[1]);
+	//dup2(sh->saved_fd[1], 1);
+	//close(sh->saved_fd[1]);
 	ft_free_list(tmp);
 }
 
@@ -44,7 +44,7 @@ void	is_real_command(t_minishell *sh)
 	if (!quote_is_closed(sh))
 		return ;
 	com = list_init(sh);
-	set_fd(sh, com);
+	//set_fd(sh, com);
 	head = com;
 	tmp = com;
 	while (com)
